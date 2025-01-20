0f8a94493edbaedae3c79b56cb7ad7f6a273d61f
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-01-20 16:19:53 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(string) {
-    const arr = [];
-    for (let i = 0; i < string.length; i++) {
-        arr.unshift(string[i]);
-    }
-    return arr.join("")
+    return [...string].reverse().join("");
 }
