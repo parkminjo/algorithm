14f19ea3ca6428e4b47e6d32473ca5ebd1553167
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-02-08 09:35:53 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,7 +1,3 @@
-function solution(start_num, end_num) {
-    const arr = [];
-    for (let i = start_num; i >= end_num; i--) {
-        arr.push(i);
-    }
-    return arr;
+function solution(start, end) {
+    return Array(start-end + 1).fill(start).map((num, idx) => num - idx);
 }
