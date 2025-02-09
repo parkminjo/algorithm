ac3702c1a67c3092146e1296e3645705d3863ca0
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-02-09 09:23:35 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,6 +1,3 @@
 function solution(str, prefix) {
-    const arr = new Array(str.length).fill(str);
-    const newArr = arr.map((s, idx) => s.slice(0, idx + 1));
-    return newArr.some(str => str === prefix) ? 1 : 0;
-    
+    return str.slice(0, prefix.length) === prefix ? 1 : 0;    
 }
