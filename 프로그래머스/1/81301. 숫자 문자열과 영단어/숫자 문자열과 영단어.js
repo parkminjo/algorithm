1a0b03220b8da64adb4b6180f1d60324f7ef84e9
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-02-24 09:30:59 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    const words = ["zero", "one", "two", "three", "four", 
+                   "five", "six", "seven", "eight", "nine"];
+
+    words.forEach((word, index) => {
+        s = s.replaceAll(word, index);
+    });
+
+    return parseInt(s);
+}
+
