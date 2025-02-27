c4e5bc766800434d1be3003070c4a4784bc5c310
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-02-27 09:49:45 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,7 +1,1 @@
-function solution(str) {
-    const arr = str.split("");
-    while (arr[0] === "0") {
-        arr.splice(0, 1);
-    }
-    return arr.join("");
-}
+const solution = (str) => String(Number(str))
