2a58cede658e97b15cbe509a768053cafe8f5b25
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-03-04 09:41:59 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.34 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,11 +1,8 @@
 function solution(n) {
-    const arr = Array(n).fill(0);
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = [];
-        
-        for (let j = 0; j < arr.length; j++) {
-            arr[i][j] = i === j ? 1 : 0
-        }
+    const result = Array.from(Array(n), () => Array(n).fill(0));
+    
+    for (let i = 0; i < result.length; i++) {
+        result[i][i] = 1;
     }
-    return arr;
+    return result;
 }
