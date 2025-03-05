79cbecd29f01573cd01a19d13786b1eeab41bd69
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-03-05 09:16:50 +0900

    [level 0] Title: 세로 읽기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,11 +1,3 @@
 function solution(str, m, c) {
-    const arrList = []
-    let answer = ""
-    for (let i = 0; i < str.length; i += m) {
-        arrList.push(str.slice(i, i + m));
-    }
-    for (arr of arrList) {
-        answer += arr[c - 1];
-    }
-    return answer;
+    return [...str].filter((_, idx) => idx % m === c - 1).join("");
 }
