5878b28dbc8fcac2d355acfc966a94f1cb44b6ff
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-03-11 09:59:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 7.43 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,22 +1,16 @@
 function solution(k, score) {
-    let fameList = [];
-    const answer = [];
-    for (let i = 0; i < score.length; i++) {
-        if (fameList.length === k) {
-            if (fameList.some(fame => fame < score[i])) {
-                const min = Math.min(...fameList);
-                fameList.splice(fameList.indexOf(min), 1);
-                fameList.push(score[i]);
-                fameList.sort((a, b) => b - a);
-                answer.push(fameList[k - 1]);
-            } else {
-                answer.push(fameList[k - 1]);
-            }
-        } else {
-            fameList.push(score[i]);
-            fameList.sort((a, b) => b - a);
-            answer.push(fameList[fameList.length - 1]);
-        };
-    }
-    return answer;
+    const stack = []
+    return score.reduce((a,c) => {
+        if(stack.length < k) {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+        }
+        else {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+            stack.shift()
+        }
+        a.push(stack[0])
+        return a
+    }, [])
 }
