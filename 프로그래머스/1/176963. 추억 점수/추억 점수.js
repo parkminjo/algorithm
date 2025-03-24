1ec255c318dd0e2807494cc2381335a17caf9b8b
MinJo Park <73922462+parkminjo@users.noreply.github.com>
2025-03-24 09:27:21 +0900

    [level 1] Title: 추억 점수, Time: 2.57 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,10 +1,5 @@
 function solution(name, yearning, photos) {
-    return photos.map(photo => {
-        let sum = 0;
-        for (let i = 0; i < photo.length; i++) {
-            name.indexOf(photo[i]) !== -1 ? sum += yearning[name.indexOf(photo[i])] : sum += 0;
-        }
-        return sum;
-    })
-    
+    return photos.map(photo => photo.reduce((acc, cur) => {
+        return acc += yearning[name.indexOf(cur)] ?? 0;
+    }, 0))
 }
