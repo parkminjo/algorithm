b729d4c2dc8ece8d0d1dd0f6cf41464f2554da0b
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-01 10:15:01 +0900

    [level 1] Title: 모의고사, Time: 2.49 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,18 @@
 function solution(answers) {
-    let student1 = [1, 2, 3, 4, 5];
-    let student2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    let student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    const getAnsweredCount = (student) => {
-        
-        return answers.map((num, i) => num === student[i % student.length] ? 1 : 0).reduce((acc, cur) => acc + cur);
-    }
-    
-    student1 = getAnsweredCount(student1)
-    student2 = getAnsweredCount(student2)
-    student3 = getAnsweredCount(student3)
-    const scores = [student1, student2, student3];
-    const maxScore = Math.max(...scores);
-    
-    const result = [];
-    scores.forEach((score, idx) => {
-        if (score === maxScore) result.push(idx + 1);
-    })
-    return result;
-} 
+    const answer = [];
+    const student1 = [1, 2, 3, 4, 5];
+    const student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    const student3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    const student1Count = answers.filter((a,i)=> a === student1[i % student1.length]).length;
+    const student2Count = answers.filter((a,i)=> a === student2[i % student2.length]).length;
+    const student3Count = answers.filter((a,i)=> a === student3[i % student3.length]).length;
+    const max = Math.max(student1Count, student2Count, student3Count);
+
+    if (student1Count === max) {answer.push(1)};
+    if (student2Count === max) {answer.push(2)};
+    if (student3Count === max) {answer.push(3)};
+
+
+    return answer;
+}
