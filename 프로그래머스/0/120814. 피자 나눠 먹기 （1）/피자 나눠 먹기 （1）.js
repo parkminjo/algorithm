79b9467377c928856e91463660508f8ad474dc9e
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-23 15:31:26 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,7 +1,3 @@
-function solution(n) {
-    const sliceOfPizza = 7;
-    
-    if (n <= 7) return 1;
-    
-    return n % 7 === 0 ? parseInt(n / 7) : parseInt(n / 7) + 1
+function solution(n) {    
+    return n % 7 === 0 ? n / 7 : parseInt(n / 7) + 1
 }
