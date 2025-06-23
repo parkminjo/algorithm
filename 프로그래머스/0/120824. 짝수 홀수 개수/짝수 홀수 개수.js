c38519e18cc5949374aefd078108f263c484b83c
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-23 15:23:03 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,8 +1,6 @@
-function solution(num_list) {
-    let countEven = 0;
-    let countOdd = 0;
-    num_list.forEach(item => {
-        item%2===0 ? countEven += 1 : countOdd += 1;
-    })
-    return [countEven, countOdd]
+function solution(numList) {
+    const evenCount = numList.filter(num => num % 2 === 0).length;
+    const oddCount = numList.filter(num => num % 2 !== 0).length;
+    
+    return [evenCount, oddCount];
 }
