e91a6b1aa6f5a819082a9f04c5b96b29e1b0d2e7
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-23 15:12:57 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,11 +1,4 @@
 function solution(n) {
-    let sum = 0;
-    
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 0) {
-            sum += i;
-        }
-    }
-    
-    return sum;
+    let half = Math.floor(n/2);
+    return half * (half + 1);
 }
