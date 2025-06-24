0df99e6e44eec10e5021140a02338a0efff50305
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-24 15:17:15 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,3 @@
 function solution(n) {
-    const numArr = String(n).split('');
-    let sum = 0;
-    
-    for (let i = 0; i < numArr.length; i++) {
-        sum += Number(numArr[i])
-    }
-    
-    return sum;
+    return n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
 }
