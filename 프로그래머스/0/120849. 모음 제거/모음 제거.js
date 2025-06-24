c9c4fdb522ae244048fc154e1ed6159eb50c5372
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-24 15:06:32 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,3 @@
 function solution(str) {
-    const arr = ['a', 'e', 'i', 'o', 'u'];
-    
-    return [...str].filter(char => !arr.includes(char)).join("");
+    return str.replace(/[aeiou]/g, '');
 }
