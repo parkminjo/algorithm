cba8c440aa657695d8dcb61497d0245c14755b84
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-24 15:50:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,12 @@
 function solution(price) {
-    if (500000 <= price) {
-        return Math.floor(price - price * 0.2);
-    } else if (300000 <= price) {
-        return Math.floor(price - price * 0.1);
-    } else if (100000 <= price) {
-        return Math.floor(price - price * 0.05);
-    } else {
-        return price;
-    }
+    if (price >= 500000)
+        return parseInt(price * (1 - 0.2));
+
+    if (price >= 300000)
+        return parseInt(price * (1 - 0.1));
+
+    if (price >= 100000)
+        return parseInt(price * (1 - 0.05));  
+
+    return price
 }
