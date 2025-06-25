40fcbc78022bd2915d911a7e60808ad876a853a0
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-25 10:32:31 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,16 +1,9 @@
 function solution(rsp) {
-    const 가위 = 2;
-    const 바위 = 0;
-    const 보 = 5;
+    const obj = {
+        2: 0,
+        0: 5,
+        5: 2
+    }
     
-    return [...rsp].map(item => {
-        if (Number(item) === 2) {
-            console.log(바위)
-            return 바위;
-        } else if (Number(item) === 0) {
-            return 보;
-        } else {
-            return 가위;
-        }
-    }).join('')
+    return [...rsp].map(item => obj[item]).join('');
 }
