f90121a7a8a15fb5c0b1cf21f3ca7c2a369a5677
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-25 11:34:13 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,10 +1,6 @@
 function solution(str, num1, num2) {
-    const num1Char = str[num1];
-    const num2Char = str[num2];
+    const strArr = [...str];
+    [strArr[num1], strArr[num2]] = [strArr[num2], strArr[num1]]
     
-    const arr = [...str];
-    arr[num1] = num2Char;
-    arr[num2] = num1Char;
-    
-    return arr.join('');
+    return strArr.join('');
 }
