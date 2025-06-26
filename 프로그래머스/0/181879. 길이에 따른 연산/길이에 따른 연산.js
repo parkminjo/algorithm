50be8e108f79c1032b66da3764a12154371ec012
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-26 10:44:21 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,4 +1,7 @@
-function solution(num_list) {
-    const answer = num_list.length >= 11 ? num_list.reduce((acc,cur) => acc + cur) : num_list.reduce((acc, cur) => acc * cur);
-    return answer;
+function solution(numList) {
+    if (numList.length >= 11) {
+        return numList.reduce((acc, cur) => acc + cur);
+    } else {
+        return numList.reduce((acc, cur) => acc * cur);
+    }   
 }
