2d170c2d4cc969fedc2cf9b6a93c53b983b5c62e
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-26 10:26:06 +0900

    [level 0] Title: 약수 구하기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,14 +1,10 @@
 function solution(n) {
-    const arr = [];
-    
+    let s = new Set();
     for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0 && n) {
-            arr.push(i);
-            if (n !== 1 && i !== n / i) {
-                arr.push(n / i);
-            }
+        if (n%i === 0) {
+            s.add(i);
+            s.add(n/i);
         }
     }
-    
-    return arr.sort((a, b) => a - b);
+    return [...s].sort((a,b)=>a-b);
 }
