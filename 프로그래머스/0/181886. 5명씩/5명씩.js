0afd566399c25e6840b49fdc7c6f4f4507b947aa
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-26 11:03:55 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,9 +1,3 @@
 function solution(names) {
-    let answer = [];
-    let limitCount = 5;
-    
-    for (let i = 0; i < names.length; i+= limitCount) {
-        answer.push(names[i]);
-    }
-    return answer;
+    return names.filter((name, idx) => idx % 5 === 0);
 }
