5339135b12ed5430a77ff043180c4ea26f32797a
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-26 10:35:39 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,12 +1,4 @@
 function solution(arr) {
-    const answerArr = [0, 0];
-    
-    for (let i = 0; i < arr.length; i++) {
-        
-        if (answerArr[0] <= arr[i]) {
-            answerArr[0] = arr[i];
-            answerArr[1] = i;
-        }
-    }
-    return answerArr;
+    const max = Math.max(...arr);
+    return [max, arr.indexOf(max)];
 }
