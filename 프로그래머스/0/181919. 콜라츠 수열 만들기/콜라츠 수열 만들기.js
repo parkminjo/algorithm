b86b905cb0ab5a3c79c7c3fcf3e7ecb7b1882eef
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-27 11:17:19 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,14 +1,15 @@
 function solution(n) {
-    let arr = [n];
-    for (let i = 0; 1 < n ;i++) {
+    const arr = [n];
+    
+    while (n > 1) {
         if (n % 2 === 0) {
-            arr.push(n/2);
+            arr.push(n / 2);
             n = n / 2;
         } else {
             arr.push(3 * n + 1);
-            n = 3 * n + 1;
+            n = 3 * n + 1
         }
     }
-    return arr;
     
+    return arr;
 }
