8b7382d710217016258e65ad7294397099697b0e
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-28 14:38:40 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,12 +1,12 @@
 function solution(a, b, c) {
-    let answer = 0;
+    const dupulicateCount = new Set([a, b, c]).size;
     
-    if ( a === b && b === c) {
-        answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
-    } else if (a === b || b === c || a === c) {
-        answer = (a + b + c) * (a**2 + b**2 + c**2 )
-    } else {
-        answer = a + b + c
+    switch (dupulicateCount) {
+        case 3:
+            return a + b + c;
+        case 2:
+            return (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+        case 1:
+            return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)  ;
     }
-    return answer;
 }
