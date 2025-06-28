31846dc2eafd1ce7f151a63e1ab543b6d77a2fd0
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-28 15:09:48 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,15 +1,5 @@
 function solution(num, k) {
-    const arr = [...String(num)];
+    const idx = String(num).indexOf(k);
     
-    let answer = -1;
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (Number(arr[i]) === k) {
-            return i + 1;
-        } else {
-            continue;
-        }
-    }
-    
-    return answer;
+    return idx === -1 ? -1 : idx + 1;
 }
