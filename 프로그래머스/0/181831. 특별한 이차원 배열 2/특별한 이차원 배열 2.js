7715cfe038ea2cd8ce150ff5a5b7ec98f594042f
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-29 09:35:36 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.00 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,11 +1,3 @@
 function solution(arr) {
-    const size = arr.length;
-
-    for(let i = 0; i < size; i++) {
-        for(let j = i + 1; j < size; j++) {
-            if(arr[i][j] !== arr[j][i]) return 0;
-        }
-    }
-
-    return 1;
+    return arr.every((numList, i) => numList.every((_, j) => arr[i][j] === arr[j][i])) ? 1 : 0;
 }
