0fb32ffa29209a9be04284743241726d54614c7c
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-29 09:25:12 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,7 +1,3 @@
 function solution(strList, ex) {
-    let answer = ""
-    strList.forEach(item => {
-        if (!item.includes(ex)) answer += item;
-    })
-    return answer;
+    return strList.filter(str => !str.includes(ex)).join('');
 }
