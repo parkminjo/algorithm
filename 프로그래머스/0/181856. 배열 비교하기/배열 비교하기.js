26fbc83ef1f431b9b0d2654e828aca284fd24ccf
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-30 11:00:36 +0900

    [level 0] Title: 배열 비교하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,11 +1,9 @@
 function solution(arr1, arr2) {
-    let answer;
-    if (arr1.length === arr2.length) {
-       const sumArr1 = arr1.reduce((acc, cur) => acc+cur);
-       const sumArr2 = arr2.reduce((acc, cur) => acc+cur);
-       answer =sumArr1 > sumArr2 ? 1 : (sumArr1 === sumArr2 ? 0 : -1);
-       return answer;
+    if (arr1.length !== arr2.length) {
+        return arr1.length > arr2.length ? 1 : -1;
+    } else {
+        if (arr1.reduce((acc, cur) => acc + cur) === arr2.reduce((acc, cur) => acc + cur)) return 0;
+        return arr1.reduce((acc, cur) => acc + cur) > arr2.reduce((acc, cur) => acc + cur) ? 1 : -1;
     }
-    answer = arr1.length > arr2.length ? 1 : -1;
-    return answer;
+    
 }
