9cd1883603eb39c917fa974aa70ee369382d3a28
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-30 11:36:29 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,6 +1,8 @@
 function solution(numList) {
-    const oddSum = numList.reduce((acc, cur, idx) => idx % 2 === 0 ? acc += cur : acc,0)
-    const evenSum = numList.reduce((acc, cur, idx) => idx % 2 !== 0 ? acc += cur : acc,0)
-    
-    return oddSum >= evenSum ? oddSum : evenSum;
+    let oddSum = 0;
+    let evenSum = 0;
+
+    numList.forEach((num, idx) => idx % 2 === 0 ? evenSum += num : oddSum += num);
+
+    return Math.max(oddSum, evenSum);
 }
