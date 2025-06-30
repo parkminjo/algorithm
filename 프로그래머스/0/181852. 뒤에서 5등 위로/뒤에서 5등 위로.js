4e6706c973c98d156b46190ef7db5080dab5f868
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-30 10:47:07 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,4 +1,3 @@
-function solution(num_list) {
-    num_list.sort((a,b) => a-b).splice(0,5);
-    return num_list;
+function solution(numList) {
+    return numList.sort((a, b) => a - b).slice(5);
 }
