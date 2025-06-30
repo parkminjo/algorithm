99fd8afa3f23399d3969a038e0748d5adb8dd97b
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-06-30 11:10:03 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,14 +1,9 @@
-const isEven = (num) => {
+function solution(a, b) {
+    if (isEven(a) && isEven(b)) return Math.abs(a - b);
+    if (!isEven(a) && !isEven(b)) return a ** 2 + b ** 2;
+    return 2 * (a + b);
+}
+
+function isEven(num) {
     return num % 2 === 0 ? true : false;
 }
-
-function solution(a, b) {
-    if (isEven(a) && isEven(b)) {
-        return Math.abs(a - b);
-    } else if (isEven(a) || isEven(b)) {
-        return Math.abs(2 * (a + b));
-    } else {
-        return Math.abs(a ** 2 + b ** 2);
-    }
-}
-
