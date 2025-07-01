14035e57980cedf441c500e6fbfc0be83b2b2362
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-01 11:12:17 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,3 @@
 function solution(str, isSuffix) {
-    const arr = Array(str.length).fill(str).map((str, idx) => str.slice(idx));
-    
-    return arr.includes(isSuffix) ? 1 : 0;
+    return Number(str.endsWith(isSuffix));
 }
