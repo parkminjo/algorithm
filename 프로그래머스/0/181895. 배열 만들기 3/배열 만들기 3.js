aeac40c98912b5cb94e47b3adfe5caf7912d40c5
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-01 10:51:36 +0900

    [level 0] Title: 배열 만들기 3, Time: 14.46 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,4 +1,3 @@
 function solution(arr, intervals) {
-    const [inter1, inter2] = intervals;
-    return [...arr.slice(inter1[0], inter1[1] + 1), ...arr.slice(inter2[0], inter2[1] + 1)]
+    return intervals.map(([start, end]) => arr.slice(start, end + 1)).flat()
 }
