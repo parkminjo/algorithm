090ee9eec719d2e9a7d6d019c5651ee568a542dd
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-01 11:01:25 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,14 +1,11 @@
 function solution(binomial) {
-    const arr = binomial.split(" ");
-    const num1 = + arr[0]
-    const num2 = + arr[2]
-    
-    switch(arr[1]) {
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-        case "*":
-            return num1 * num2;
+    const [a, op, b] = binomial.split(' ')
+    switch (op) {
+        case '+':
+            return +a + +b;
+        case '-':
+            return +a - +b;
+        case '*':
+            return +a * +b;
     }
 }
