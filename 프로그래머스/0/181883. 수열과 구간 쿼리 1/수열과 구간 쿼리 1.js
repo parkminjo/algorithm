3022894822ca26e7f5786564d2102bf269d7c0de
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-03 11:30:54 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 24.89 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,14 +1,8 @@
 function solution(arr, queries) {
-    for(let i = 0; i < queries.length; i++) {
-        const [s, e] = queries[i];
-        
-        arr = arr.map((item, idx) => {
-            if (s <= idx && e >= idx) {
-                return item + 1;
-            } else {
-                return item;
-            }
-        })
-    }
+    queries.forEach(([s, e]) => {
+        for (let i = s; i <= e; i++) {
+            arr[i] += 1;
+        }
+    })
     return arr;
 }
