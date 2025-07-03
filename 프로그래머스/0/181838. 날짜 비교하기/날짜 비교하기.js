b1352264374eae9d39af8fc01b1886863a0fb051
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-03 10:54:02 +0900

    [level 0] Title: 날짜 비교하기, Time: 4.47 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,15 +1,3 @@
 function solution(date1, date2) {
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    
-    if (year1 < year2) return 1;
-    if (year1 > year2) return 0;
-    
-    if (month1 < month2) return 1;
-    if (month1 > month2) return 0;
-    
-    if (day1 < day2) return 1;
-    if (day1 > day2) return 0;
-    
-    return 0;
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
