f4a9796ff2f4dd46124fdd73a5b475107e0edf61
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-04 10:38:29 +0900

    [level 0] Title: 1로 만들기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,3 +1,13 @@
 function solution(numList) {
-    return numList.map(v => v.toString(2).length - 1).reduce((a, c) => a + c);
+    return numList.map(num => getCount(num)).reduce((acc, cur) => acc + cur);
+}
+
+function getCount(num) {
+    let count = 0;
+    
+    for (let i = num; num > 1; i--) {
+        num = num % 2 === 0 ? num / 2 : (num - 1) / 2
+        count += 1;
+    }
+    return count;
 }
