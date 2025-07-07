2f861a45f73308a6860b742c1fe9352a0f532944
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-07 10:25:56 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,11 +1,10 @@
 function solution(str, pat) {
     let count = 0;
-    
-    [...str].forEach((char, idx) => {
-        if (char === pat[0] && str.slice(idx, idx + pat.length) === pat) {
-            count += 1;
+    for (let i = 0; i <= str.length - pat.length; i++) {
+        if (str.slice(i, i + pat.length) === pat) {
+            count++;
         }
-    })
-    
+    }
+
     return count;
 }
