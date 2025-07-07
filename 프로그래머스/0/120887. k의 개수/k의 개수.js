e10d5098c0e86f7a2314ed72547b7515ae5ec8fa
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-07 11:07:56 +0900

    [level 0] Title: k의 개수, Time: 18.23 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,14 +1,9 @@
 function solution(i, j, k) {
-    let count = 0;
-    const target = String(k);
+    let a = '';
     
-    for (let num = i; num <=j; num++) {
-        const numStr = String(num);
-        
-        for (let char of numStr) {
-            if (char === target) count++;
-        }
+    for(i; i<=j; i++){
+        a += i;
     }
-    
-    return count;
+
+    return a.split(k).length - 1;
 }
