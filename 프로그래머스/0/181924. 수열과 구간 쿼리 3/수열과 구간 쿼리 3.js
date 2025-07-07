e23683e3f0e2e7e3b678f54e18a752d28da509d8
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-07 10:19:13 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.87 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
     queries.forEach(([i, j]) => {
-        const num1 = arr[i]
-        arr[i] = arr[j]
-        arr[j] = num1
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     })
     return arr;
 }
