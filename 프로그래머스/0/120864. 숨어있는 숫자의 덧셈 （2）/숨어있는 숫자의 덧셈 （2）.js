881bc1b750cb0d7c6f58592bf92d9c6c5ef64504
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-09 10:51:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,18 +1,3 @@
 function solution(str) {
-    let answer = 0;
-    let temp = '';
-    
-    [...str].forEach(char => {
-        if (char >= '0' && char <= '9') {
-            
-            temp += char;
-        } else {
-            answer += Number(temp);
-            temp = '';
-        }
-    })
-    
-    if (temp) answer += Number(temp);
-    
-    return answer;
+    return str.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
