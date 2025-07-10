3c5f9a0191f2d29da6cea7e67b6ee14d0456d224
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-10 10:42:42 +0900

    [level 0] Title: 리스트 자르기, Time: 0.12 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,8 +1,14 @@
 function solution(n, slicer, numList) {
     const [a, b, c] = slicer;
     
-    if (n === 1) return numList.slice(0, b + 1);
-    if (n === 2) return numList.slice(a);
-    if (n === 3) return numList.slice(a, b + 1);
-    return numList.slice(a, b + 1).filter((num, idx) => idx % c === 0);
+    switch(n) {
+        case 1:
+            return numList.slice(0, b + 1);
+        case 2:
+            return numList.slice(a);
+        case 3:
+            return numList.slice(a, b + 1);
+        case 4:
+            return numList.slice(a, b + 1).filter((num, idx) => idx % c === 0);
+    }
 }
