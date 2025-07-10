e878407ea6fb027ba29802d6d4091088551d665a
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-10 10:29:38 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,15 +1,3 @@
 function solution(arr) {
-    let count = 0;
-    
-    arr.forEach(num => {
-        if (num === 7) count += 1;
-        
-        if (num > 10) {
-            String(num).split('').forEach(str => {
-                if (Number(str) === 7) count += 1;
-            })
-        }
-    })
-    
-    return count;
+    return arr.join('').split('7').length - 1;
 }
