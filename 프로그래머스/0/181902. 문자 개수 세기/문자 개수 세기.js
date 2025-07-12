e116ae0a0022bf23135e1b63a92b5d17608ebb5f
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-12 11:17:35 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,15 +1,13 @@
 function solution(str) {
-    const result = Array(52).fill(0);
+    var answer = [];
+    const al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const a = [];
+    a.length = 52;
+    a.fill(0);
 
-    for(let i = 0; i < str.length; i++){
-        const alpha = str.charCodeAt(i);
-        
-        if (alpha >= 65 && alpha <= 90){
-            result[alpha - 65]++;
-        } else {
-            result[alpha - 71]++;
-        }
-    }
-    
-    return result;
+    str.split("").map((n)=>{
+        a[al.indexOf(n)]+=1
+    })
+
+    return a;
 }
