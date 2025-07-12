34c226ee048e99ce8fdd36eeb8164c6c0932219f
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-12 10:54:24 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,18 +1,15 @@
 function solution(n) {
     const answer = [];
-
-    for (let i = 2; i * i <= n; i++) {
-        if (n % i === 0) {
-            answer.push(i);
-            while (n % i === 0) {
-                n /= i;
-            }
-        }
+    let i = 2;
+    
+    while (i <= n) {
+      if (n % i === 0) {
+        answer.push(i)     
+        n = n / i
+      } else {
+        i++        
+      }
     }
 
-    if (n > 1) {
-        answer.push(n);
-    }
-
-    return answer;
+    return [...new Set(answer.sort((a, b) => a - b))]
 }
