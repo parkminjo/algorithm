70b5f803383d3656de1b4b4623a77db8518f36a2
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-15 10:57:13 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.94 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,17 +1,10 @@
 function solution(arr, k) {
     const set = new Set(arr);
-    const newArr = [...set];
-    const newArrLength = newArr.length;
+    const result = [...set].slice(0, k);
     
-    if (newArr.length > k) {
-        for (let i = 0; i < newArrLength - k; i++) {
-            newArr.pop();
-        }
-    } else {
-        for (let i = 0; i < k - newArrLength; i++) {
-            newArr.push(-1);
-        }
+    while (result.length !== k) {
+        result.push(-1);
     }
     
-    return newArr;
+    return result;
 }
