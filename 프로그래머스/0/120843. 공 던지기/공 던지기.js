9720edd510c9b1f476b8cfd03a037a20678c8ab0
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-15 10:11:05 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,8 +1,3 @@
 function solution(numbers, k) {
-    let idx = 0;
-    for (let i = 1; i < k; i++) {
-        idx = (idx + 2) % numbers.length;
-    }
-    
-    return numbers[idx];
+    return numbers[(--k * 2) % numbers.length];
 }
