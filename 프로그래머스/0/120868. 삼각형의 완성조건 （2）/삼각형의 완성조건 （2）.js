c15e25ff2d1d74e96ee80184296fe35465f2a963
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-16 10:38:50 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,7 +1,3 @@
 function solution(sides) {
-    const [a, b] = sides;
-    const max = a + b - 1;
-    const min = Math.abs(a - b) + 1;
-    return max - min + 1;
+    return Math.min(...sides) * 2 - 1;
 }
-
