0fb622ec4b53142369ca0667aa4ed86fa17eb32a
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-17 10:49:44 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,28 +1,22 @@
 function solution(keyinput, board) {
-    const direction = {
-        up: [0, 1],
-        down: [0, -1],
-        right: [1, 0],
-        left: [-1, 0],
-    }
+    const result = [0, 0];
     
-    const [width, height] = board;
-    const xLimit = Math.floor(width / 2);
-    const yLimit = Math.floor(height / 2);
-    
-    let x = 0;
-    let y = 0;
-    
-    for (let key of keyinput) {
-        const [a, b] = direction[key];
-        const nextX = x + a;
-        const nextY = y + b;
-        
-        if (Math.abs(nextX) <= xLimit && Math.abs(nextY) <= yLimit) {
-            x = nextX;
-            y = nextY;
+    for (let k of keyinput) {
+        switch (k) {
+            case 'right':
+                if (result[0] < Math.floor(board[0] / 2)) result[0]++;
+                break;
+            case 'left':
+                if (-result[0] < Math.floor(board[0] / 2)) result[0]--;
+                break;
+            case 'up':
+                if (result[1] < Math.floor(board[1] / 2)) result[1]++;
+                break;
+            case 'down':
+                if (-result[1] < Math.floor(board[1] / 2)) result[1]--;
+                break;
         }
     }
     
-    return [x, y];
+    return result;
 }
