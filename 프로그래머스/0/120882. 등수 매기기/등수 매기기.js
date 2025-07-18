d0d7d2eb45838a50801f6eeb42f1bdb468063cfc
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-18 10:36:47 +0900

    [level 0] Title: 등수 매기기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,6 +1,6 @@
 function solution(score) {
     const average = score.map(([en, math]) => (en + math) / 2);
-    const sortedAverage = score.map(([en, math]) => (en + math) / 2).sort((a, b) => b- a);
+    const sortedAverage = [...average].sort((a, b) => b- a);
     
     return average.map(score => sortedAverage.indexOf(score) + 1);
     
