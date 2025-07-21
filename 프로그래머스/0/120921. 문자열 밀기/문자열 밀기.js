e81813711d5ed23099b5acb06087e1504ec93be5
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-21 11:54:39 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,13 +1,3 @@
 function solution(A, B) {
-    const queue = [...A];
-    
-    for (let count = 0; count < A.length; count++) {
-        if (queue.join('') === B) {
-            return count;
-        }
-        const char = queue.pop();
-        queue.unshift(char);
-    }
-    
-    return -1;
+    return (B + B).indexOf(A);
 }
