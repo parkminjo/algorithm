063c9b2e889ba275688267e75c693309002523c1
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-22 10:57:37 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,14 +1,3 @@
 function solution(numList, n) {
-    numList.sort((a, b) => {
-        const distA = Math.abs(a - n);
-        const distB = Math.abs(b - n);
-        
-        if (distA === distB) {
-            return b - a;
-        }
-        
-        return distA - distB;
-    });
-    
-    return numList;
+    return numList.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
