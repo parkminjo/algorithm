03e0b688e2a65cf528455666bfb284521ca7b260
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-07-25 10:40:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,15 +1,11 @@
 function solution(numer1, denom1, numer2, denom2) {
-    const lcm = getLCM(denom1, denom2);
-    const numer = (lcm / denom1) * numer1 + (lcm / denom2) * numer2;
-    const gcd = getGCD(numer, lcm);
-    
-    return [numer / gcd, lcm / gcd];
+    let numer = numer1 * denom2 + numer2 * denom1;
+    let denom = denom1 * denom2;
+    let gcd = getGCD(denom, numer);
+
+    return [numer / gcd, denom / gcd];
 }
 
-function getLCM(a, b) {
-    return (a * b) / getGCD(a, b);
-}
-
-function getGCD(a, b) {
-    return b === 0 ? a : getGCD(b, a % b);
+function getGCD(a, b){
+    return (a % b) ? getGCD(b, a % b) : b;
 }
