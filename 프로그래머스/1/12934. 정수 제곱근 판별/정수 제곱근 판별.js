d9e23a1c0edba4297e771a207a565c3e9fea873e
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-08-05 09:35:37 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    const x = Math.ceil(Math.sqrt(n));
-    return x**2 === n ? (x + 1)**2 : -1;
+    const x = Math.sqrt(n);
+    return Number.isInteger(x) ? (x + 1)**2 : -1;
 }
