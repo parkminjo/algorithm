39017e5ea0b9806f88acee051c1cb3f3babcb501
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-08-05 09:09:37 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,15 +1,9 @@
 function solution(n) {
     let result = 0;
     
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0 && n / i === i) {
-            result += i;
-            continue;
-        }
-        
+    for (let i = 1; i <= n; i++) {
         if (n % i === 0) {
-            result += i;
-            result += n / i;
+            result += i
         }
     }
     
