020c8c533518d5d9fc563bc09ce9946c5bc8ecdc
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-08-06 10:52:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,15 @@
 function solution(num) {
     let count = 0;
     
-    while (num > 1) {
+    while (num > 1 && count !== 500) {
         if (num % 2 === 0) {
             num = num / 2;
             count++;
-        } else if (num % 2 !== 0) {
+        } else {
             num = num * 3 + 1;
             count++;
         }
     }
     
-    if (count > 500) return -1;
-    return count;
+    return num === 1 ? count : -1;
 }
