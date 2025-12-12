c01dcec031dac5f63a436878a38dcae019c5a51b
parkminjo <73922462+parkminjo@users.noreply.github.com>
2025-12-12 12:32:03 +0900

    [level 1] Title: 실패율, Time: 5286.99 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,28 +1,18 @@
 function solution(N, stages) {
-    const failureLates = [];
+    const result = [];
     
     for (let i = 1; i <= N; i++) {
-        let count = 0;
-        let total = 0;
+        const reachCount = stages.filter(stage => i === stage).length;
+        const total = stages.filter(stage => i <= stage).length;
+        const failure = reachCount / total;
         
-        for (let j = 0; j < stages.length; j++) {
-            if (i === stages[j]) {
-                count += 1;
-            }
-            
-            if (i <= stages[j]) {
-                total += 1;
-            }
-        }
-        
-        const failure = count / total;
-        failureLates.push([i, failure]);
+        result.push([i, failure]);
     }
     
-    failureLates.sort((a, b) => {
+    result.sort((a, b) => {
         if (b[1] === a[1]) return a[0] - b[0];
         return b[1] - a[1];
-    });
+    })
     
-    return failureLates.map(item => item[0]);
+    return result.map(item => item[0]);
 }
